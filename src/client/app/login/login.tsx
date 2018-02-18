39e86f52b338f7c2d2aa1141187e15086908c9c9
Blake Zimmerman <blake.zimmerman@icloud.com>
2018-02-18 13:31:41 -0500

    Refactor register and login page to reuse code

--- src/client/app/login/login.tsx
+++ src/client/app/login/login.tsx
@@ -5,7 +5,9 @@ import { State } from 'client/store/rootReducer';
 import { ActionDispatcher } from 'client/shared/reduxUtils';
 import { NewUser } from 'models/users';
 import { LOGIN_REQUEST } from './login.reducer';
+import { InsertOneWriteOpResult } from 'mongodb';
 import Link from 'redux-first-router-link';
+import AccountForm from './accountForm';
 
 interface Props {
   pending: boolean;
@@ -14,72 +16,22 @@ interface Props {
   requestLogin: ActionDispatcher<NewUser>;
 }
 
-interface LocalState {
-  userName: string;
-  password: string;
-}
+const Login = (props: Props) => {
+  const loginButton = (requestedUser: NewUser) => (event: React.MouseEvent<HTMLButtonElement>) =>
+    props.requestLogin(requestedUser);
 
-class Login extends React.Component<Props, LocalState> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      userName: '',
-      password: ''
-    };
-  }
-
-  isDisabled = () => !(this.state.userName && this.state.password);
-
-  updateUserName = (event: React.FormEvent<HTMLInputElement>) =>
-    this.setState({
-      userName: event.currentTarget.value
-    })
-
-  updatePassword = (event: React.FormEvent<HTMLInputElement>) =>
-    this.setState({
-      password: event.currentTarget.value
-    })
-
-  loginButton = (requestedUser: NewUser) => (event: React.MouseEvent<HTMLButtonElement>) =>
-    this.props.requestLogin(requestedUser)
-
-  render() {
-    return (
-      <div className={styles.container}>
-        <h2>Sign in</h2>
-        {this.props.error && <h3 className={styles.error}>{this.props.error}</h3>}
-        <div className={styles.inputs}>
-          <label>
-            {'Username:'}
-            <input
-              type='text'
-              value={this.state.userName}
-              onChange={this.updateUserName}
-            />
-          </label>
-          <label>
-            {'Password:'}
-            <input
-              type='password'
-              value={this.state.password}
-              onChange={this.updatePassword}
-            />
-          </label>
-          <button
-            onClick={this.loginButton(this.state)}
-            disabled={this.isDisabled()}
-          >
-            Log In
-          </button>
-        </div>
-        <div>
-          {'Not Registered? '}
-          <Link to='/register'>Click here to make an account!</Link>
-        </div>
+  return (
+    <div className={styles.container}>
+      <h2>Sign in</h2>
+      {props.error && <h3 className={styles.error}>{props.error}</h3>}
+      <AccountForm submitText='Log In' submitFn={loginButton}/>
+      <div>
+        {'Not Registered? '}
+        <Link to='/register'>Click here to make an account!</Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const mapState = (state: State) => ({ ...state.login.loginRequest });
 
--- src/client/app/login/register.tsx
+++ src/client/app/login/register.tsx
@@ -7,6 +7,7 @@ import { NewUser } from 'models/users';
 import { REGISTER_REQUEST } from './login.reducer';
 import { InsertOneWriteOpResult } from 'mongodb';
 import Link from 'redux-first-router-link';
+import AccountForm from './accountForm';
 
 interface Props {
   pending: boolean;
@@ -15,76 +16,26 @@ interface Props {
   requestRegistration: ActionDispatcher<NewUser>;
 }
 
-interface LocalState {
-  userName: string;
-  password: string;
-}
+const Register = (props: Props) => {
+  const registerButton = (requestedUser: NewUser) => (event: React.MouseEvent<HTMLButtonElement>) =>
+    props.requestRegistration(requestedUser);
 
-class Register extends React.Component<Props, LocalState> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      userName: '',
-      password: ''
-    };
-  }
-
-  isDisabled = () => !(this.state.userName && this.state.password);
-
-  updateUserName = (event: React.FormEvent<HTMLInputElement>) =>
-    this.setState({
-      userName: event.currentTarget.value
-    })
-
-  updatePassword = (event: React.FormEvent<HTMLInputElement>) =>
-    this.setState({
-      password: event.currentTarget.value
-    })
-
-  registerButton = (requestedUser: NewUser) => (event: React.MouseEvent<HTMLButtonElement>) =>
-    this.props.requestRegistration(requestedUser)
-
-  render() {
-    return (
-      <div className={styles.container}>
+  return (
+    <div className={styles.container}>
         <h2>Create an Account</h2>
-        {this.props.error && !this.props.result &&
-          <h3 className={styles.error}>{this.props.error}</h3>
+        {props.error && !props.result &&
+          <h3 className={styles.error}>{props.error}</h3>
         }
-        {this.props.result && !this.props.error &&
+        {props.result && !props.error &&
           <h3 className={styles.success}>
             {'Registration Success! '}
             <Link to='/login'>Click here to proceed to login.</Link>
           </h3>
         }
-        <div className={styles.inputs}>
-          <label>
-            {'Username '}
-            <input
-              type='text'
-              value={this.state.userName}
-              onChange={this.updateUserName}
-            />
-          </label>
-          <label>
-            {'Password '}
-            <input
-              type='password'
-              value={this.state.password}
-              onChange={this.updatePassword}
-            />
-          </label>
-          <button
-            onClick={this.registerButton(this.state)}
-            disabled={this.isDisabled()}
-          >
-            Create Account
-          </button>
-        </div>
+        <AccountForm submitText='Create Account' submitFn={registerButton}/>
       </div>
-    );
-  }
-}
+  );
+};
 
 const mapState = (state: State) => ({ ...state.login.registerRequest });
 
